34f48f71466521ef683a4f69e739fb60d369bb3f
Junhuai <850217160@qq.com>
2018-09-05 15:41:22 +0800

    ++

--- Design4/memory.c
+++ Design4/memory.c
@@ -10,21 +10,19 @@
 
 void CreateMemory(GtkWidget* notebook)
 {
+    GtkWidget *table;
 
-    GtkWidget *vbox;  
-    GtkWidget *text_label;
-
-    // 创建盒装容器并添加
-    vbox = gtk_vbox_new(FALSE, 10);
-    // gtk_container_add(GTK_CONTAINER(notebook), vbox);
-    gtk_container_border_width(GTK_CONTAINER(vbox), 150);
-
-    text_label = gtk_label_new("");
-    gtk_box_pack_start(GTK_BOX(vbox), text_label, FALSE, FALSE, 10);
-    gtk_label_set_markup(
-        GTK_LABEL(text_label), "<span font_desc='18'>0-9的循环：3</span>");
-    gtk_widget_show(text_label);
-
+    // 需要表格布局 创建10行10列的表格
+    table = gtk_table_new(10, 10, TRUE);
     /* 将该标签页加入到notebook中 */
-	gtk_notebook_append_page(GTK_NOTEBOOK(notebook), vbox, gtk_label_new("内存"));
+	gtk_notebook_append_page(GTK_NOTEBOOK(notebook), table, gtk_label_new("内存"));
+
+    // 创建frame来装各种控件
+    GtkWidget *CPU_frame1 = gtk_frame_new("内存详细情况");
+    GtkWidget *CPU_frame2 = gtk_frame_new("内存使用曲线");
+    GtkWidget *CPU_frame3 = gtk_frame_new("交换分区使用曲线");
+
+    gtk_table_attach_defaults(GTK_TABLE(table), CPU_frame1, 0, 3, 0, 10);
+    gtk_table_attach_defaults(GTK_TABLE(table), CPU_frame2, 3, 10, 0, 6);
+    gtk_table_attach_defaults(GTK_TABLE(table), CPU_frame3, 3, 10, 6, 10);
 }
